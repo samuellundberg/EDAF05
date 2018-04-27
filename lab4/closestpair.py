23c4c6e276397f2a7312b612e2bd8e5fbc1599d2
samuellundberg <37873752+samuellundberg@users.noreply.github.com>
2018-04-27 12:15:40 +0200

    Add files via upload

--- lab4/closestpair.py
+++ lab4/closestpair.py
@@ -2,7 +2,7 @@ import sys
 import math
 
 points = []
-best = [int(2**32), (None, None)]
+best = [int(2**32), None, None]
 
 
 class Point(object):
@@ -21,10 +21,10 @@ def make_point(index, x, y):
 def xy_sort(list_of_points, q):
     x_p = sorted(list_of_points, key=lambda p: p.x)
     if q == 1:                          # vi vill bara ge ny indexering första gången
-        i = 1
+        ii = 1
         for tp in x_p:
-            tp.index = i
-            i += 1
+            tp.index = ii
+            ii += 1
     return x_p
 
 
@@ -39,7 +39,8 @@ def test_pair(p1, p2):
     d = distance(p1, p2)
     if d < best[0]:
         best[0] = d
-        best[1] = p1, p2
+        best[1] = p1
+        best[2] = p2
 
 
 def read_file(file):
@@ -51,14 +52,14 @@ def read_file(file):
             raw_p, raw_x, raw_y = line.split()
             if 'e' in raw_x:                             # e^-4 ish hanteras
                 x1, x2 = raw_x.split('e')
-                raw_x = float(x1) ** float(x2)
+                raw_x = float(x1) * 10 ** float(x2)
             if 'e' in raw_y:                             # e^-4 ish hanteras
                 y1, y2 = raw_y.split('e')
-                raw_y = float(y1) ** float(y2)
+                raw_y = float(y1) * 10 ** float(y2)
             make_point(int(raw_p), float(raw_x), float(raw_y))
 
 
-def dc_alg(p, xp):
+def dc_alg(p):
     cut = int(len(p)/2)
     if cut == 1:
         if abs(p[0].x - p[1].x) < best[0] and abs(p[0].y - p[1].y) < best[0]:
@@ -68,6 +69,8 @@ def dc_alg(p, xp):
             ind = p[1].index
             dx = abs(x_points[ind].x - p[1].x)
             dy = abs(x_points[ind].y - p[1].y)
+            # if p[1].x == 16.3:
+            #    print(delta, dx, dy)
             while dx < delta:
                 if dy < delta:
                     test_pair(x_points[ind], p[1])
@@ -75,13 +78,16 @@ def dc_alg(p, xp):
                 ind += 1
                 if ind < len(points):
                     dx = abs(x_points[ind].x - p[1].x)
-                    dy = abs(x_points[ind].y - p[1].x)
+                    dy = abs(x_points[ind].y - p[1].y)
+                # if p[1].x == 16.3:
+                #   print(delta, dx, dy, x_points[ind].x)
                 else:
                     break
 
             ind = p[1].index
             dx2 = abs(x_points[ind].x - p[0].x)
             dy2 = abs(x_points[ind].y - p[0].y)
+
             while dx2 < delta:
                 if dy2 < delta:
                     test_pair(x_points[ind], p[0])
@@ -93,18 +99,46 @@ def dc_alg(p, xp):
                 else:
                     break
 
-    else:
-        pl = xp[0:cut]          # för långsam?
-        pr = xp[cut:2*cut]
-        dc_alg(pl, pl)
-        dc_alg(pr, pr)
+    if cut == 0:
+        if p[0].index < len(points):        # titta till höger, upp och ner, då slipper vi titta vänster
+            delta = best[0]
+            ind = p[0].index
+            dx = abs(x_points[ind].x - p[0].x)
+            dy = abs(x_points[ind].y - p[0].y)
+            while dx < delta:
+                if dy < delta:
+                    test_pair(x_points[ind], p[0])
+                ind += 1
+                if ind < len(points):
+                    dx = abs(x_points[ind].x - p[0].x)
+                    dy = abs(x_points[ind].y - p[0].y)
+                else:
+                    break
+
+    else:           # här är det fritt fram at göra fler 0(n) opperationer, typ sortera på y..
+        pl = p[0:cut]
+        pr = p[cut:]
+        dc_alg(pl)
+        dc_alg(pr)
 
 
 text_file = sys.argv[1]
 read_file(text_file)
 num = len(points)
 x_points = xy_sort(points, 1)
-dc_alg(points, x_points)
-print(sys.argv[1][:-4])     # name
-print(num)          # number
-print(best[0])      # dist
+dc_alg(x_points)
+if '.txt' in sys.argv[1]:
+    name = sys.argv[1].replace('.txt', ':')
+else:
+    nl = []
+    for i in sys.argv[1]:
+        nl.append(i)
+    nl.append(':')
+    name = nl.__str__()
+    name = name.replace('[', '')
+    name = name.replace(']', '')
+    name = name.replace(', ', '')
+    name = name.replace("'", '')
+
+print(name, num, best[0])       # python3
+# print name, num, best[0]        # python2
